2c330ad4124599b583f77d22f6231df151d22580
beyyzanuryilmaz <120269114+beyyzanuryilmaz@users.noreply.github.com>
2023-03-12 16:18:29 +0300

    Add files via upload

---
+++ lib/screens/nalan.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Nalan extends StatelessWidget {
+  const Nalan({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
